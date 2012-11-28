981cdbee08dbd8661da5361da225d2c4e226f810
HU Yang Gang <yangg.hu@gmail.com>
2012-11-29 03:08:37 +0800

    sync a working copy of the API from appfog plus a bunch of bug fixes :). More critical issues remains still, such as potential SQL injection vulnerabilities.

--- api/index.php
+++ api/index.php
@@ -1,36 +1,22 @@
 <?php
+// Comment the following two lines when we are online
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header('Content-Type:application/json; charset=UTF-8');
 require_once('db_lib.php');
 $oDB = new db;
 
-
-function print_r_html($arr)
+foreach(array('method', 'auth_token', 'isbn', 'outdate', 'duedate', 'callback', 'firstname',
+    'lastname', 'company', 'phone', 'email', 'address', 'city', 'state', 'postcode', 
+    'userid', 'quantity', 'sortby', 'page', 'pageitems', 'catid') AS $var)
 {
-?><pre><?
-    print_r($arr);
-?></pre><?
+    $$var = '';
+    if(isset($_GET[$var]))
+    {
+        $$var = $_GET[$var];
+    }
 }
-
-$method     = $_GET['method'];
-$auth_token = $_GET['auth_token'];
-$isbn       = $_GET['isbn'];
-$outdate    = $_GET['outdate'];
-$duedate    = $_GET['duedate'];
-$callback   = $_GET['callback'];
-$firstname  = $_GET['firstname'];
-$lastname   = $_GET['lastname'];
-$company    = $_GET['company'];
-$phone      = $_GET['phone'];
-$email      = $_GET['email'];
-$address    = $_GET['address'];
-$city       = $_GET['city'];
-$state      = $_GET['state'];
-$postcode   = $_GET['postcode'];
-$userid     = $_GET['userid'];
-$quantity   = $_GET['quantity'];
-$sortby     = $_GET['sortby'];
-$page       = $_GET['page'];
-$pageitems = $_GET['pageitems'];
 
 /*
 $method= 'getAllBooks';//'getBookByIsbn';//'getAllBooks';
@@ -62,18 +48,18 @@ if ($method == 'getBookByIsbn') {
             $row_set['pubdate']     = $row['pubdate'];
             $row_set['img']         = $row['img'];
             $row_set['stock']       = $row['stock'];
-			$row_set['visit']       = $row['visit'];
+            $row_set['visit']       = $row['visit'];
             $row_set['title']       = urlencode($row['title']);
-			$row_set['cat_name']    = urlencode($row['cat_name']);
+            $row_set['cat_name']    = urlencode($row['cat_name']);
             $row_set['description'] = urlencode(str_replace('"', '&rdquo;', $row['description']));
             $row_set['author']      = urlencode(str_replace('"', '&rdquo;', $row['author']));
             
         }
         //print_r($row_set[]);
         $json = json_encode($row_set);
-		
-		$oDB->update('books'," isbn10='$isbn' OR  isbn13 = '$isbn' ", " visit = visit + 1 " );
-		  
+        
+        $oDB->update('books'," isbn10='$isbn' OR  isbn13 = '$isbn' ", " visit = visit + 1 " );
+          
         //print_r_html($row_set);
         if ($callback != '')
             echo $callback . '(' . str_replace(PHP_EOL, '<br>', urldecode($json)) . ')';
@@ -111,16 +97,16 @@ if ($method == 'getBookByIsbn') {
                 $row_set['pubdate']     = $row['pubdate'];
                 $row_set['img']         = $row['img'];
                 $row_set['stock']       = $row['stock'];
-				$row_set['visit']       = $row['visit'];
-				$row_set['cat_name']    = urlencode($row['cat_name']);
+                $row_set['visit']       = $row['visit'];
+                $row_set['cat_name']    = urlencode($row['cat_name']);
                 $row_set['title']       = urlencode($row['title']);
                 $row_set['description'] = urlencode(str_replace('"', '&rdquo;', $row['description']));
                 $row_set['author']      = urlencode(str_replace('"', '&rdquo;', $row['author']));
                 
             }
             $json = json_encode($row_set);
-			
-				$oDB->update('books'," isbn10='$isbn' OR  isbn13 = '$isbn' ", " visit = visit + 1 " );
+            
+                $oDB->update('books'," isbn10='$isbn' OR  isbn13 = '$isbn' ", " visit = visit + 1 " );
             //print_r_html($row_set);
             if ($callback != '')
                 echo $callback . '(' . str_replace(PHP_EOL, '<br>', urldecode($json)) . ')';
@@ -135,14 +121,20 @@ if ($method == 'getBookByIsbn') {
 //end getbook
 
 else if ($method == 'getCategories') {
-    $result = $oDB->select("SELECT cat_name FROM category order by cat_name"); //mysql_query("SELECT * FROM books WHERE isbn10=7505715666 OR  isbn13 =7505715666 ");       
+    $result = $oDB->select("SELECT catid,cat_name FROM category order by cat_name"); //mysql_query("SELECT * FROM books WHERE isbn10=7505715666 OR  isbn13 =7505715666 ");       
+
     if (mysqli_num_rows($result)) {
+        $row_set = array();
+        $i       = 0;
         while ($row = mysqli_fetch_assoc($result)) {
-			$row_set['cat_name']    = urlencode($row['cat_name']);           
+            $row_set[$i]['cat_name'] = urlencode($row['cat_name']);           
+            $row_set[$i]['catid']    = $row['catid']; 
+            $i++;
         }
+
         //print_r($row_set[]);
         $json = json_encode($row_set);
-	  
+      
         //print_r_html($row_set);
         if ($callback != '')
             echo $callback . '(' . str_replace(PHP_EOL, '<br>', urldecode($json)) . ')';
@@ -150,7 +142,7 @@ else if ($method == 'getCategories') {
             echo str_replace(PHP_EOL, '<br>', urldecode($json));
         
  }
-
+}
 else if ($method == 'deleteBook') {
     if ($oDB->in_table(" loan ", " isbn ='$isbn' and quantity >0")) {
         if ($callback != '')
@@ -170,33 +162,33 @@ else if ($method == 'deleteBook') {
 
 else if ($method == 'getAllBooks') {
     //$result = $oDB->select("SELECT * FROM books LEFT OUTER JOIN loan ON ( books.isbn10 = loan.isbn OR books.isbn13 = loan.isbn ) ");
-	 $sSql =  "SELECT books.*,category.*
-					FROM books
-					LEFT OUTER JOIN category ON books.catid = category.catid "  ;  
-	
-	if($sortby){
-		if($sortby=='record'){
-			$sSql =  "SELECT books.*,category.*, count(loan.loanid) as count 
-					FROM books
-					LEFT OUTER JOIN category ON books.catid = category.catid
-					LEFT JOIN loan ON ( loan.isbn = books.isbn10
-					OR loan.isbn = books.isbn13 )  group by loan.loanid
-					order by count desc";
-		}
-		else
-		{
-			 
-		   $sSql =  $sSql .  " order by $sortby ";
-		   
-		}	
-	}
-	if($page && $pageitems){
-		$start =  ($page -1) * $page;
-		$end =  $pageitems * $page - 1;
-		$sSql =  $sSql .  " LIMIT $start ,  $end" ;
-	}
+     $sSql =  "SELECT books.*,category.*
+                    FROM books
+                    LEFT JOIN category ON books.catid = category.catid "  ;  
+    
+    if($sortby){
+        if($sortby=='record'){
+            $sSql =  "SELECT books.*,category.*, count(loan.loanid) as count 
+                    FROM books
+                    LEFT OUTER JOIN category ON books.catid = category.catid
+                    LEFT JOIN loan ON ( loan.isbn = books.isbn10
+                    OR loan.isbn = books.isbn13 )  group by loan.loanid
+                    order by count desc";
+        }
+        else
+        {
+             
+           $sSql =  $sSql .  " order by $sortby ";
+           
+        }   
+    }
+    if($page && $pageitems){
+        $start =  ($page -1) * $page;
+        $end =  $pageitems * $page - 1;
+        $sSql =  $sSql .  " LIMIT $start ,  $end" ;
+    }
  
-	//print_r($sSql);
+    //print_r($sSql);
     $result = $oDB->select($sSql);
     if (mysqli_num_rows($result)) {
         $row_set = array();
@@ -207,7 +199,8 @@ else if ($method == 'getAllBooks') {
             $row_set[$i]['pubdate']     = $row['pubdate'];
             $row_set[$i]['img']         = $row['img'];
             $row_set[$i]['stock']       = $row['stock'];
-			$row_set['cat_name']    = urlencode($row['cat_name']);
+            $row_set[$i]['cat_name']    = urlencode($row['cat_name']);
+            $row_set[$i]['catid']       = $row['catid'];
             $row_set[$i]['title']       = urlencode($row['title']);
             $row_set[$i]['description'] = urlencode(str_replace('"', '&rdquo;', $row['description']));
             $row_set[$i]['author']      = urlencode(str_replace('"', '&rdquo;', $row['author']));
@@ -223,7 +216,39 @@ else if ($method == 'getAllBooks') {
         
     }
     
-} else if ($method == 'loanBook') {
+} else if ($method == 'getBooksByCategory') {
+    //$result = $oDB->select("SELECT * FROM books LEFT OUTER JOIN loan ON ( books.isbn10 = loan.isbn OR books.isbn13 = loan.isbn ) ");
+    $sSql =  "SELECT * FROM books LEFT JOIN category on books.catid = category.catid WHERE books.catid='$catid'"  ;  
+    $row_set = array();
+
+    //print_r($sSql);
+    $result = $oDB->select($sSql);
+    if (mysqli_num_rows($result)) {
+        $i       = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $row_set[$i]['isbn10']      = $row['isbn10'];
+            $row_set[$i]['isbn13']      = $row['isbn13'];
+            $row_set[$i]['pubdate']     = $row['pubdate'];
+            $row_set[$i]['img']         = $row['img'];
+            $row_set[$i]['stock']       = $row['stock'];
+            $row_set[$i]['cat_name']    = urlencode($row['cat_name']);
+            $row_set[$i]['catid']       = $row['catid'];
+            $row_set[$i]['title']       = urlencode($row['title']);
+            $row_set[$i]['description'] = urlencode(str_replace('"', '&rdquo;', $row['description']));
+            $row_set[$i]['author']      = urlencode(str_replace('"', '&rdquo;', $row['author']));
+            $i++;
+        }
+    }
+
+    //print_r($row_set[]);
+    $json = json_encode($row_set);
+    //print_r_html($row_set);
+    if ($callback != '')
+        echo $callback . '(' . str_replace(PHP_EOL, '<br>', urldecode($json)) . ')';
+    else
+        echo str_replace(PHP_EOL, '<br>', urldecode($json));
+}
+else if ($method == 'loanBook') {
     if ($oDB->in_table(" loan ", " isbn ='$isbn' and userid = $userid")) {
         $oDB->update('loan', " isbn ='$isbn' and userid = $userid", " out_date = '$outdate' , quantity = quantity + $quantity ");
         $oDB->update('books', " isbn10='$isbn' OR  isbn13 = '$isbn'", " stock = stock - $quantity ");
@@ -337,7 +362,4 @@ else if ($method == 'getUsers') {
             echo str_replace(PHP_EOL, '<br>', urldecode($json));
         
     }
-    
 }
-
-?>
